8afbf192816c11d6090f73253b923c3fe0a120f6
Bcxw <67139204+WZW13821252731@users.noreply.github.com>
2021-04-21 15:55:06 +0800

    modify_1

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'tunnel_light' 
+ * Target:  'tunnel_light' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
